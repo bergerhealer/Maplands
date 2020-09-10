dc5ae63985903f47a465153f4b29a094bc4ad2e5
Irmo van den Berge <irmo.vandenberge@ziggo.nl>
2020-09-10 03:06:57 +0200

    Add changemap permission

--- src/main/java/com/bergerkiller/bukkit/maplands/Permission.java
+++ src/main/java/com/bergerkiller/bukkit/maplands/Permission.java
@@ -6,6 +6,7 @@ import org.bukkit.permissions.PermissionDefault;
 public class Permission extends PermissionEnum {
     public static final Permission COMMAND_MAP = new Permission("maplands.command.map", PermissionDefault.OP, "Gives the player the maplands map item");
     public static final Permission COMMAND_GIVE = new Permission("maplands.command.give", PermissionDefault.OP, "Gives the maplands map item to another player");
+    public static final Permission CHANGE_MAP = new Permission("maplands.changemap", PermissionDefault.OP, "Allows the player to make changes to the map in the interactive menu");
 
     private Permission(final String node, final PermissionDefault permdefault, final String desc) {
         super(node, permdefault, desc);
